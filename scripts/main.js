63be9bba66750b9f48e8b9833c1ee96a5e6baafd
NowyMe <nowy.meepho@hva.nl>
2022-12-01 14:00:29 +0100

    vercel

--- scripts/main.js
+++ scripts/main.js
@@ -39,7 +39,7 @@ console.log('Hello, world!');
 //   })
 //   .catch((err) => console.error(err));
 
-fetch('../public/data/f1rankings.json')
+fetch('../data/f1rankings.json')
   .then((response) => response.json())
   .then((data) => {
     const rakingArray = data.response;
@@ -279,7 +279,7 @@ L.tileLayer(
 ).addTo(map);
 
 var myIcon = L.icon({
-  iconUrl: 'public/images/flag2.png',
+  iconUrl: '../images/flag2.png',
   iconSize: [40, 40],
   iconAnchor: [5, 40],
   popupAnchor: [15, -33],
