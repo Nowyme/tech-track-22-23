c5ce4618338ff78195739c9fe4c6fcb04e2d142d
NowyMe <nowy.meepho@hva.nl>
2022-12-01 13:39:45 +0100

    test

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ import '../styles/style.css';
 import * as d3 from 'd3';
 import gsap from 'gsap';
 import { svg } from 'd3';
+import * as m from './map.js';
 
 console.log('Hello, world!');
 
@@ -54,12 +55,12 @@ fetch('../data/f1rankings')
       //HTML markup
       const markup = `<li>
       <h2>${driverPostition}</h2>
-			<img src="${driverImg}" alt="${driverName}">
+    	<img src="${driverImg}" alt="${driverName}">
       <h3>${driverName}</h3>
-			<p>${driverPoints}</p>
+    	<p>${driverPoints}</p>
       </li>`;
 
-      document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
+      document.querySelector('.listD').insertAdjacentHTML('beforeend', markup);
     });
 
     const top4Driver = rakingArray.slice(0, 5);
@@ -142,3 +143,155 @@ d3.select('#labels')
   .join('text')
   .attr('y', (d) => yScale(d.driver) + yScale.bandwidth() / 2)
   .text((d) => d.driver);
+
+//leaft let
+
+const coords = [
+  {
+    name: 'Bahrain International Circuit',
+    longlat: [33.763489, -118.193649],
+    winner: 'Charles Leclerc',
+  },
+  {
+    name: 'Jeddah Corniche Circuit',
+    longlat: [21.61264, 39.10806],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Albert Park Street Circuit',
+    longlat: [-37.84452, 144.94651],
+    winner: 'Charles Leclerc',
+  },
+  {
+    name: 'Autodromo Enzo e Dino Ferrari',
+    longlat: [44.34425020604998, 11.719518298678329],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Miami International Autodrome',
+    longlat: [25.956777117916822, -80.23150098991948],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Circuit de Barcelona-Catalunya',
+    longlat: [41.56830708666628, 2.2571188084700045],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Circuit de Monaco',
+    longlat: [43.73486454785762, 7.421411392106805],
+    winner: 'Sergio Perez',
+  },
+
+  {
+    name: 'Baku City Circuit',
+    longlat: [40.37282816855186, 49.85295629315864],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Circuit Gilles Villeneuve',
+    longlat: [45.501528363448784, -73.52803410120104],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Silverstone Circuit',
+    longlat: [52.073295900672065, -1.0148545602668675],
+    winner: 'Carlos Sainz Jr.',
+  },
+  {
+    name: 'Red Bull Ring',
+    longlat: [47.22315229010509, 14.76129439965457],
+    winner: 'Charles Leclerc',
+  },
+
+  {
+    name: 'Circuit Paul Ricard',
+    longlat: [43.25108100419511, 5.793438680986409],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Hungaroring',
+    longlat: [47.58132751634226, 19.25095392155147],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Circuit van Spa-Francorchamps',
+    longlat: [50.436576937266786, 5.971899795181605],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Circuit Zandvoort',
+    longlat: [52.38760986011846, 4.544008951045924],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Autodromo Nazionale Monza',
+    longlat: [45.61970904196297, 9.287476258857936],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Marina Bay Street Circuit',
+    longlat: [1.2944204060642202, 103.86663581786028],
+    winner: 'Sergio Perez',
+  },
+  {
+    name: 'Circuit Suzuka',
+    longlat: [34.84562870078928, 136.53838357031526],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Circuit of the Americas',
+    longlat: [30.134209628415075, -97.63584037264167],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Autódromo Hermanos Rodríguez',
+    longlat: [19.405068316076758, -99.09237308256166],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Autódromo José Carlos Pace',
+    longlat: [-23.701332343097416, -46.697707638342],
+    winner: 'George Russel',
+  },
+  {
+    name: 'Yas Marina Circuit',
+    longlat: [24.473429941219667, 54.60418912684601],
+    winner: 'Max Verstappen',
+  },
+];
+
+var map = L.map('map').setView([51.505, -0.09], 3);
+
+L.tileLayer(
+  'https://api.mapbox.com/styles/v1/maarwaarom/clb2etvk200d115oktiskpzer/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFhcndhYXJvbSIsImEiOiJjbGF0aWw2aHMwMWJwM3FsNGcyZDJ1NnY0In0.yVsF9VnckgXKerO30_gogA',
+  {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">Mapbox</a>',
+  }
+).addTo(map);
+
+var myIcon = L.icon({
+  iconUrl: 'images/flag2.png',
+  iconSize: [40, 40],
+  iconAnchor: [5, 40],
+  popupAnchor: [15, -33],
+});
+
+// var marker = L.marker([51.5, -0.09]).addTo(map);
+// var markerD = L.marker([20.5, -0.09]).addTo(map);
+// marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
+// markerD.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
+
+coords.forEach(({ name, longlat, winner }) => {
+  L.marker(longlat, { icon: myIcon })
+    .addTo(map)
+    .bindPopup(`<h2>${name}</h2> <p>Winner of the GP: <b>${winner}</b></p>`);
+});
---
+++ scripts/map.js
@@ -0,0 +1,120 @@
+const coords = [
+  {
+    name: 'Bahrain International Circuit',
+    longlat: [33.763489, -118.193649],
+    winner: 'Charles Leclerc',
+  },
+  {
+    name: 'Jeddah Corniche Circuit',
+    longlat: [21.61264, 39.10806],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Albert Park Street Circuit',
+    longlat: [-37.84452, 144.94651],
+    winner: 'Charles Leclerc',
+  },
+  {
+    name: 'Autodromo Enzo e Dino Ferrari',
+    longlat: [44.34425020604998, 11.719518298678329],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Miami International Autodrome',
+    longlat: [25.956777117916822, -80.23150098991948],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Circuit de Barcelona-Catalunya',
+    longlat: [41.56830708666628, 2.2571188084700045],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Circuit de Monaco',
+    longlat: [43.73486454785762, 7.421411392106805],
+    winner: 'Sergio Perez',
+  },
+
+  {
+    name: 'Baku City Circuit',
+    longlat: [40.37282816855186, 49.85295629315864],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Circuit Gilles Villeneuve',
+    longlat: [45.501528363448784, -73.52803410120104],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Silverstone Circuit',
+    longlat: [52.073295900672065, -1.0148545602668675],
+    winner: 'Carlos Sainz Jr.',
+  },
+  {
+    name: 'Red Bull Ring',
+    longlat: [47.22315229010509, 14.76129439965457],
+    winner: 'Charles Leclerc',
+  },
+
+  {
+    name: 'Circuit Paul Ricard',
+    longlat: [43.25108100419511, 5.793438680986409],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Hungaroring',
+    longlat: [47.58132751634226, 19.25095392155147],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Circuit van Spa-Francorchamps',
+    longlat: [50.436576937266786, 5.971899795181605],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Circuit Zandvoort',
+    longlat: [52.38760986011846, 4.544008951045924],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Autodromo Nazionale Monza',
+    longlat: [45.61970904196297, 9.287476258857936],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Marina Bay Street Circuit',
+    longlat: [1.2944204060642202, 103.86663581786028],
+    winner: 'Sergio Perez',
+  },
+  {
+    name: 'Circuit Suzuka',
+    longlat: [34.84562870078928, 136.53838357031526],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Circuit of the Americas',
+    longlat: [30.134209628415075, -97.63584037264167],
+    winner: 'Max Verstappen',
+  },
+
+  {
+    name: 'Autódromo Hermanos Rodríguez',
+    longlat: [19.405068316076758, -99.09237308256166],
+    winner: 'Max Verstappen',
+  },
+  {
+    name: 'Autódromo José Carlos Pace',
+    longlat: [-23.701332343097416, -46.697707638342],
+    winner: 'George Russel',
+  },
+  {
+    name: 'Yas Marina Circuit',
+    longlat: [24.473429941219667, 54.60418912684601],
+    winner: 'Max Verstappen',
+  },
+];
